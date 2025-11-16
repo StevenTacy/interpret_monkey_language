61dc662ea8cbdc0453d00089b797ea11182c0d35
stevie <zzzsteven718@gmail.com>
2025-11-16 23:07:53 +0800

    interpret_monkey_language | feat: lexer part, tokenize the standard input and print output

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s, this is the stevie interpreter adventure, i'm tokenize the input\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
---
+++ token/token.go
@@ -0,0 +1,70 @@
+package token
+
+// record the token type
+type TokenType string
+
+// a token has its own type and its literal value
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifier & literal
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	ASTERISK = "*"
+	BANG     = "!"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+
+	TRUE   = "TRUE"
+	FALSE  = "FALSE"
+	IF     = "IF"
+	ELSE   = "ELSE"
+	RETURN = "RETURN"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+/**
+ * check if the current char is identifier, if in the keywords map return the corresponding
+ * TokenType, if not just inform user it is a self defined identifier
+ */
+func CheckUpIdentifier(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
