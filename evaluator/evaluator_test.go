ddab95019995cf0506551dd830146d722db49b27
stevie <zzzsteven718@gmail.com>
2025-11-27 23:25:06 +0800

    feat: REPL | create IF-ELSE expression value parsing

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -76,6 +76,32 @@ func TestBangOperator(t *testing.T) {
 	}
 }
 
+func TestIfElseExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if (true) { 10 }", 10},
+		{"if (false) { 10 }", nil},
+		{"if (1) { 10 }", 10},
+		{"if (1 < 2) { 10 }", 10},
+		{"if (1 > 2) { 10 }", nil},
+		{"if (1 > 2) { 10 } else { 20 }", 20},
+		{"if (1 < 2) { 10 } else { 20 }", 10},
+	}
+
+	for _, test := range tests {
+		evaluated := testEval(test.input)
+		integer, ok := test.expected.(int)
+
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -106,6 +132,15 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 
 	if res.Value != expected {
 		t.Errorf("object has wrong value, got %t, want %t", res.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("expect obj is null, got %T (%+v)", obj, obj)
 		return false
 	}
 
